e27b52475a4d2c328d48c9a6a5be4de1722efed5
alexwander <ryndyuk.alexander@gmail.com>
2016-03-28 20:31:48 +0300

    playing with some basik script

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,51 @@
 /**
  * Created by alexandr on 27.03.2016.
  */
+
+
+$(document).ready(function() {
+    $('.answer').hide();
+    $('.main h2').click(function() {
+        var $answer = $(this).next('.answer');
+        if ($answer.is(':hidden')) {
+            $answer.slideDown();
+        } else {
+        }
+    }); // end click
+}); // end of ready()
+
+
+$(document).ready(function() {
+    $('.answer').hide();
+    $('.main h2').click(function() {
+        var $answer = $(this).next('.answer');
+        if ($answer.is(':hidden')) {
+        } else {
+        }
+    }); // end click
+}); // end of ready()
+
+$(document).ready(function() {
+    $('.answer').hide();
+    $('.main h2').click(function() {
+        var $answer = $(this).next('.answer');
+        if ( ) {
+        } else {
+        }
+    }); // end click
+}); // end of ready()
+$(document).ready(function() {
+    $('.answer').hide();
+    $('.main h2').click(function() {
+        var $answer = $(this).next('.answer');
+        if ($answer.is(':hidden')) {
+            $answer.slideDown();
+            $(this).addClass('close');
+        } else {
+            $answer.fadeOut();
+            $(this).removeClass('close');
+        }
+    }); // end click
+}); // end of ready()
+</script>
+
