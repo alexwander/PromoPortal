cc8145801da82304c39a77066ec8ed91ad98a278
alexwander <ryndyuk.alexander@gmail.com>
2016-04-03 21:49:24 +0300

    created fancy box

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 /**
  * Created by alexandr on 27.03.2016.
  */
-
+var WINDOW = window.WINDOW || {};
 
 $(document).ready(function() {
     $('html').dblclick(function() {
@@ -10,3 +10,28 @@ $(document).ready(function() {
     $('a').mouseover(function() {
     }); // end mouseover
 }); // end ready
+
+
+WINDOW.fancyBox = function(){
+    if($('.fancybox').length > 0 || $('.fancybox-media').length > 0 || $('.fancybox-various').length > 0){
+
+        $(".fancybox").fancybox({
+            padding : 0,
+            beforeShow: function () {
+                this.title = $(this.element).attr('title');
+                this.title = '<h4>' + this.title + '</h4>' + '<p>' + $(this.element).parent().find('img').attr('alt') + '</p>';
+            },
+            helpers : {
+                title : { type: 'inside' },
+            }
+        });
+
+        $('.fancybox-media').fancybox({
+            openEffect  : 'none',
+            closeEffect : 'none',
+            helpers : {
+                media : {}
+            }
+        });
+    }
+}
