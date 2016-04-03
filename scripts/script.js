52f67044de7faaea23a137bf6d070ea4bb43aef8
alexwander <ryndyuk.alexander@gmail.com>
2016-04-03 21:58:32 +0300

    add contact form

--- scripts/script.js
+++ scripts/script.js
@@ -53,3 +53,29 @@ WINDOW.listenerMenu = function(){
         $('#navigation-mobile').slideUp(350, 'easeOutExpo');
     });
 }
+
+
+WINDOW.contactForm = function(){
+    $("#contact-submit").on('click',function() {
+        $contact_form = $('#contact-form');
+
+        var fields = $contact_form.serialize();
+
+        $.ajax({
+            type: "POST",
+            url: "_include/php/contact.php",
+            data: fields,
+            dataType: 'json',
+            success: function(response) {
+
+                if(response.status){
+                    $('#contact-form input').val('');
+                    $('#contact-form textarea').val('');
+                }
+
+                $('#response').empty().html(response.html);
+            }
+        });
+        return false;
+    });
+}
