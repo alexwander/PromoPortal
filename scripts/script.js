27af2b7f5fea0e9aa187707b761e68c708d57f81
alexwander <ryndyuk.alexander@gmail.com>
2016-04-08 22:38:34 +0300

    mouse control f

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('html').dblclick(function() {
+        alert('ouch');
+    }); // end double click
+    $('a').mouseover(function() {
+    }); // end mouseover
+}); // end ready
