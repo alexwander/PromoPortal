e57f57f6b9d29d7e64c9379e43d3c16d455e9ff2
alexwander <ryndyuk.alexander@gmail.com>
2016-04-09 23:31:29 +0300

    playing with hide

--- scripts/script.js
+++ scripts/script.js
@@ -25,3 +25,15 @@ $('#theElement').on({
 $('ul').on('click', function() {
     $(this).css('text-decoration': 'line-through');
 }
+
+$('.main h2').click(function() {
+    var $answer = $(this).next('.answer');
+    if ($answer.is(':hidden')) {
+        $answer.slideDown();
+        $(this).addClass('close');
+    } else {
+        $answer.fadeOut();
+        $(this).removeClass('close');
+    }
+}); // end click
+}); // end of ready()
