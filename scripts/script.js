f7c5f1526582522d00fd745980b2eb59156ee0f3
alexwander <ryndyuk.alexander@gmail.com>
2016-04-11 21:57:05 +0300

    event handler

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
+$(document).ready(function() {
+    $('#open').click(function(evt) {
+    }); // end click
+}); // end ready
 $('#button').click(function() {
     $('#instructions').fadeToggle(500);
 }); // end click
