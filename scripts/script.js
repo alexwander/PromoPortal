6ae704b44bd2e0227c8b3a17c77f8eddf8169691
alexwander <ryndyuk.alexander@gmail.com>
2016-04-12 22:53:13 +0300

    cleanup

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#open').click(function(evt) {
-    }); // end click
-}); // end ready
-$('#button').click(function() {
-    $('#instructions').fadeToggle(500);
-}); // end click
