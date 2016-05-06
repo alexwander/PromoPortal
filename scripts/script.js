2629057cd58f3e67f7da946c6f94bc4e368db9ee
alexwander <ryndyuk.alexander@gmail.com>
2016-05-06 17:09:11 +0300

    add hide func

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
     $('.sm').smartmenus({
         showFunction: function($ul, complete) {
             $ul.slideDown(250, complete);
+        },
+        hideFunction: function($ul, complete) {
+            $ul.slideUp(250, complete);
         }
     }); // end smartmenus
 }); // end ready
-
