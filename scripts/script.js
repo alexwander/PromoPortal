55cd0fde410e5fb0e9a3c6b5ed6b07fdd37eec40
alexwander <ryndyuk.alexander@gmail.com>
2016-05-13 22:51:25 +0300

    add animation

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,17 @@ $(document).ready(function() {
              'easeInBounce'); // end animate
          }); // end click
      }); // end ready
+
+$('#photo').width(0).height(0).css('opacity',0);
+ $('#caption').hide();
+ $('#photo').animate(
+     {
+     width: '200px',
+         height: '100px',
+         opacity: 1
+     },
+ 1000,
+     function() {
+     $('#caption').fadeIn(1000);
+     }
+ ); // end animate
