db6cdd6a9ccc0ce32d887064d2473add541df2aa
alexwander <ryndyuk.alexander@gmail.com>
2016-05-15 22:54:33 +0300

    del unused func

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,4 @@ $(document).ready(function() {
 
 });
 
-$(document).ready(function() {
-    $('#animate').click(function() {
-        $(this).animate(
-            {
-                width: '40px',
-             height: '40px'
-         },
-         1000,
-             'easeInBounce');
-         });
-     });
 
