e74c813f89cdbffc3d6bda84e2b77d944672d993
alexwander <ryndyuk.alexander@gmail.com>
2016-05-17 22:35:51 +0300

    del unused

--- scripts/script.js
+++ scripts/script.js
@@ -2,23 +2,3 @@ $(document).ready(function() {
 
 });
 
-$(function() {
-    // set up the news accordion on the lower page
-    $("#newsSection").accordion({ header: "h4" });
-
-    // create the image rotator
-    setInterval("rotateImages()", 2000);
-});
-
-function rotateImages() {
-    var oCurPhoto = $('#photoShow div.current');
-    var oNxtPhoto = oCurPhoto.next();
-    if (oNxtPhoto.length == 0)
-        oNxtPhoto = $('#photoShow div:first');
-
-    oCurPhoto.removeClass('current').addClass('previous');
-    oNxtPhoto.css({ opacity: 0.0 }).addClass('current').animate({ opacity: 1.0 }, 1000,
-        function() {
-            oCurPhoto.removeClass('previous');
-        });
-}
