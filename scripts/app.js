df24f69f865151b89b2c57e461aa9fbc89f9794d
alexwander <ryndyuk.alexander@gmail.com>
2016-12-31 20:21:12 +0200

    debug refactoring

--- scripts/app.js
+++ scripts/app.js
@@ -1,44 +1,41 @@
-
 var app = angular.module("portfolio", ['ngRoute']);
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
-
     $routeProvider
-        .when('/main', {
-            templateUrl: 'main.html',
-            controller: 'MainCtrl'
-        })
-        .when('/about', {
-            templateUrl: 'about.html',
-            controller: 'MainCtrl'
-        })
-        .when('/blog', {
-            templateUrl: 'blog.html',
-            controller: 'BlogCtrl'
-        })
-        .when('/ourwork', {
-            templateUrl: 'ourwork.html',
-            controller: 'MainCtrl'
-        })
-        .when('/contact', {
-            templateUrl: 'contact.html',
-            controller: 'ContactCtrl'
-        })
-        .otherwise({redirectTo:'/main'});
-
+    .when('/main', {
+        templateUrl: 'main.html',
+        controller: 'MainCtrl'
+    })
+    .when('/about', {
+        templateUrl: 'about.html',
+        controller: 'MainCtrl'
+    })
+    .when('/blog', {
+        templateUrl: 'blog.html',
+        controller: 'BlogCtrl'
+    })
+    .when('/ourwork', {
+        templateUrl: 'ourwork.html',
+        controller: 'MainCtrl'
+    })
+    .when('/contact', {
+        templateUrl: 'contact.html',
+        controller: 'ContactCtrl'
+    })
+    .otherwise({redirectTo:'/main'});
 
     $locationProvider.html5Mode(true);
 }]);
+    app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+        console.log('hi');
 
-app.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
-    console.log('hi');
+    }]);
+     app.controller('BlogCtrl', ['$scope', '$http', function ($scope, $http) {
+        console.log('hi blog');
 
-}]);
-app.controller('BlogCtrl', ['$scope', '$http', function ($scope, $http) {
-    console.log('hi blog');
+    }]);
+      app.controller('ContactCtrl', ['$scope', '$http', function ($scope, $http) {
+        console.log('hi contact');
 
-}]);
-app.controller('ContactCtrl', ['$scope', '$http', function ($scope, $http) {
-    console.log('hi contact');
-
-}]);
+    }]);
+    
